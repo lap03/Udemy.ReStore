8f8971f847f7ff11bb81bca1476f140db7d03991
tinle <tinle7347@gmail>
2024-05-13 11:19:03 +0700

    add same get in controller

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -29,5 +29,18 @@ namespace API.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetData()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
     }
 }
